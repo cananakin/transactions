8f625606d0c7e76507270bae8fee89f7634fec69
canan <e.cananakin@gmail.com>
2020-09-16 02:25:22 +0100

    edit

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ app.use(express.static(__dirname + '/dist/my-app'));
 app.get('/*', function (req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.sendFile(__dirname + '/dist/my-app/index.html');
-    const url = `https://sandbox-reporting.rpdpymnt.com`;
-
-  
-    request(url).pipe(res);
+});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
 app.use(cors());
 app.listen(process.env.PORT || 8080);
