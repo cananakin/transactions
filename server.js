5b0872b52999afcfaa626268c47362d98c8ebefa
canan <e.cananakin@gmail.com>
2020-09-16 01:39:39 +0100

    edit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const cors = require('cors');
 
 app.use(express.static(__dirname + '/dist/my-app'));
 
@@ -9,7 +9,7 @@ app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/dist/my-app/index.html'
   );
 });
-
+app.use(cors());
 app.listen(process.env.PORT || 8080);
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
